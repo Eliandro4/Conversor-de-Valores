ec66c9c820e23c357b39c729124a8103554cba30
Eliandro4 <153942958+Eliandro4@users.noreply.github.com>
2024-08-28 12:50:37 -0300

    Projeto

---
+++ Conversores.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace Conversores
+{
+    public class Conv
+    {
+        public void AscHex(string asc)
+        {
+            char[] asm = asc.ToCharArray();
+            int i = 0;
+            string[] amm = new string[asm.Length];
+            foreach (char cha in asm)
+            {
+                amm[i] = ($"{Convert.ToInt32(cha):X}");
+                i = i + 1;
+            }
+
+            Console.WriteLine(string.Join(" ", amm));
+        }
+        public void HexAsc(string hexa)
+        {
+            string[] asm = hexa.Split(' ');
+            int i = 0;
+            string[] amm = new string[asm.Length];
+            foreach (string hex in asm)
+            {
+                int valor = Convert.ToInt32(hex, 16);
+                string stringValue = Char.ConvertFromUtf32(valor);
+                amm[i] = stringValue;
+                i++;
+            }
+            Console.WriteLine(string.Join("", amm));
+        }
+        public void AscBi(string asc)
+        {
+            StringBuilder binaryString = new StringBuilder();
+            foreach (char c in asc)
+            {
+                binaryString.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+                binaryString.Append(" ");
+            }
+            Console.WriteLine(binaryString.ToString().Trim());
+        }
+        public void BiAsc(string input)
+        {
+            string[] binaryValues = input.Split(' ');
+            StringBuilder text = new StringBuilder();
+            foreach (string binary in binaryValues)
+            {
+            int decimalValue = Convert.ToInt32(binary, 2);
+            text.Append((char)decimalValue);
+            }
+            Console.WriteLine(text.ToString());
+        }
+        public void AscMor(string asc)
+        {
+
+        }
+        public void MorAsc(string asc)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Conversores;
+
+namespace Prog
+{
+    internal class Pgconv
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0] == "-h")
+                {
+                    string[] helpmsg = { "Texto para Hexadecimal", "Hexadecimal para Texto", "Texto para Binário", "Binário para Texto", "Texto para Código Morse", "Código Morse para Texto" };
+                    int i = 1;
+                    foreach (string msg in helpmsg)
+                    {
+                        Console.WriteLine($"\"-{i}\". {msg}");
+                        i++;
+                    }
+                }
+                else
+                {
+                    if (args.Length > 1)
+                    {
+                        var con = new Conv();
+                        string arms = string.Join(" ", args.Skip(1)); // Pega somente os "argumentos" após o parametro
+                        switch (args[0])
+                        {
+                        case "-1": //Texto para Hex
+                            con.AscHex(arms);
+                            con = null;
+                            break;
+                        case "-2": //Hex para Texto
+                            con.HexAsc(arms);
+                            con = null;
+                            break;
+                        case "-3": // Texto para Binario
+                            con.AscBi(arms);
+                            con = null;
+                            break;
+                        case "-4": // Binario para Texto
+                            con.BiAsc(arms);
+                            con = null;
+                            break;
+                        case "-5": //Texto para Codigo morse
+                            con.AscMor(arms);
+                            con = null;
+                            break;
+                        case "-6": //Codigo morse para texto
+                            con.MorAsc(arms);
+                            con = null;
+                            break;
+                        }                    
+                    }
+                }
+            }
+        }
+    }
+}
